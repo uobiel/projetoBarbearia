8a665e458f2d861e48f1fd6781d7c59d2b8f34d9
Gabriel Antunes <gabrielantunesemp@gmail.com>
2023-12-28 14:59:30 -0300

    ajuste

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// Use o cors antes das rotas
+// Configuração do CORS
 app.use(cors({
-  origin: '*',  // ou ajuste para a origem específica do seu aplicativo
+  origin: '*', // Permitir todas as origens. Ajuste conforme necessário.
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
+  optionsSuccessStatus: 204, // É importante incluir isso para preflight requests.
 }));
 
+// Configuração do MySQL
 const connection = mysql.createConnection({
   host: 'aws.connect.psdb.cloud',
   user: 'qe5zstvy8h41ncylbibx',
@@ -27,61 +30,60 @@ const connection = mysql.createConnection({
 
 const tabelaNoBanco = 'agendamentos';
 
+// Rota para salvar no banco
 app.post('/salvar-no-banco', (req, res) => {
-    const { nome, dataNasc, hora } = req.body;
+  const { nome, dataNasc, hora } = req.body;
 
-    const sql = `INSERT INTO ${tabelaNoBanco} (nome, dataNasc, hora) VALUES (?, ?, ?)`;
-    const values = [nome, dataNasc, hora];
+  const sql = `INSERT INTO ${tabelaNoBanco} (nome, dataNasc, hora) VALUES (?, ?, ?)`;
+  const values = [nome, dataNasc, hora];
 
-    connection.query(sql, values, (error, results) => {
-        if (error) {
-            console.error('Erro ao inserir no banco de dados:', error);
-            res.status(500).json({ success: false, error: 'Erro interno no servidor' });
-            return;
-        }
+  connection.query(sql, values, (error, results) => {
+    if (error) {
+      console.error('Erro ao inserir no banco de dados:', error);
+      res.status(500).json({ success: false, error: 'Erro interno no servidor' });
+      return;
+    }
 
-        console.log('Dados inseridos com sucesso:', results);
-        res.json({ success: true });
-    });
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
-
-app.get('/buscar-horarios', (req, res) => {
-  const sql = `SELECT * FROM ${tabelaNoBanco}`;
-  
-  connection.query(sql, (error, results) => {
-      if (error) {
-          console.error('Erro ao buscar horários do banco de dados:', error);
-          res.status(500).json({ error: 'Erro interno no servidor' });
-          return;
-      }
-
-      console.log('Horários obtidos com sucesso:', results);
-      res.json(results);
+    console.log('Dados inseridos com sucesso:', results);
+    res.json({ success: true });
   });
 });
 
+// Rota para buscar horários
+app.get('/buscar-horarios', (req, res) => {
+  const sql = `SELECT * FROM ${tabelaNoBanco}`;
+
+  connection.query(sql, (error, results) => {
+    if (error) {
+      console.error('Erro ao buscar horários do banco de dados:', error);
+      res.status(500).json({ error: 'Erro interno no servidor' });
+      return;
+    }
+
+    console.log('Horários obtidos com sucesso:', results);
+    res.json(results);
+  });
+});
+
+// Rota para remover horário
 app.delete('/remover-horario/:id', (req, res) => {
   const horarioId = req.params.id;
 
   const sql = `DELETE FROM ${tabelaNoBanco} WHERE id = ?`;
 
   connection.query(sql, [horarioId], (error, results) => {
-      if (error) {
-          console.error('Erro ao remover horário do banco de dados:', error);
-          res.status(500).json({ error: 'Erro interno no servidor' });
-          return;
-      }
+    if (error) {
+      console.error('Erro ao remover horário do banco de dados:', error);
+      res.status(500).json({ error: 'Erro interno no servidor' });
+      return;
+    }
 
-      console.log('Horário removido com sucesso:', results);
-      res.json({ success: true });
+    console.log('Horário removido com sucesso:', results);
+    res.json({ success: true });
   });
 });
 
-
+// Rota para marcar como concluído
 app.post('/marcar-como-concluido/:id', (req, res) => {
   const horarioId = req.params.id;
 
@@ -98,4 +100,9 @@ app.post('/marcar-como-concluido/:id', (req, res) => {
     console.log('Horário marcado como concluído com sucesso:', results);
     res.json({ success: true });
   });
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
