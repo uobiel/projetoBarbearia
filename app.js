2e79df339d1d29e39f8420f7a05100ab06450af8
Gabriel Antunes <gabrielantunesemp@gmail.com>
2023-12-28 14:47:35 -0300

    ajuste dominio

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://127.0.0.1:5500',
+  origin: 'https://projeto-barbearia-indol.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
